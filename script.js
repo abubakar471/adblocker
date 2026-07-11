f8027998925c5d896d558f5862a15d0535dfbfef
abubakar471 <abdurjoy2001@gmail.com>
2026-07-12 02:15:34 +0600

    feat: implement on search query video load and video title

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     console.log("site loaded")
+    
+    const recommendedVideos = [
+        {
+            title: "How I met your mother | Season 2 | Episode 1",
+            slug: "how-i-met-your-mother-s2-e1",
+            href: "./index.html?v=how-i-met-your-mother-s2-e1",
+            videoUrl: "./assets/video.mp4",
+            thumbnail: "./assets/e1.jpg"
+        },
+        {
+            title: "How I met your mother | Season 2 | Episode 2",
+            slug: "how-i-met-your-mother-s2-e2",
+            href: "./index.html?v=how-i-met-your-mother-s2-e2",
+            videoUrl: "./assets/video.mp4",
+            thumbnail: "./assets/e2.jpeg"
+        },
+        {
+            title: "How I met your mother | Season 2 | Episode 3",
+            slug: "how-i-met-your-mother-s2-e3",
+            href: "./index.html?v=how-i-met-your-mother-s2-e3",
+            videoUrl: "./assets/video.mp4",
+            thumbnail: "./assets/e3.jpeg"
+        },
+        {
+            title: "How I met your mother | Season 2 | Episode 4",
+            slug: "how-i-met-your-mother-s2-e4",
+            href: "./index.html?v=how-i-met-your-mother-s2-e4",
+            videoUrl: "./assets/video.mp4",
+            thumbnail: "./assets/e4.jpg"
+        },
+        {
+            title: "How I met your mother | Season 2 | Episode 5",
+            slug: "how-i-met-your-mother-s2-e5",
+            href: "./index.html?v=how-i-met-your-mother-s2-e5",
+            videoUrl: "./assets/video.mp4",
+            thumbnail: "./assets/e5.jpeg"
+        }
+    ];
 
    
     const player = document.querySelector(".video-player");
     const thumbnail = document.querySelector(".video-thumbnail");
-
-
-    const recommendedVideos = [
-        {
-            title: "How I met your mother | Season 2 | Episode 1",
-            href: "./index.html?v=how-i-met-your-mother-s2-e1",
-            videoUrl: "./assets/video.mp4",
-            thumbnail: "./assets/video.jpg"
-        },
-        {
-            title: "How I met your mother | Season 2 | Episode 2",
-            href: "./index.html?v=how-i-met-your-mother-s2-e2",
-            videoUrl: "./assets/video.mp4",
-            thumbnail: "./assets/video.jpg"
-        },
-        {
-            title: "How I met your mother | Season 2 | Episode 3",
-            href: "./index.html?v=how-i-met-your-mother-s2-e3",
-            videoUrl: "./assets/video.mp4",
-            thumbnail: "./assets/video.jpg"
-        },
-        {
-            title: "How I met your mother | Season 2 | Episode 4",
-            href: "./index.html?v=how-i-met-your-mother-s2-e4",
-            videoUrl: "./assets/video.mp4",
-            thumbnail: "./assets/video.jpg"
-        }
-    ];
-
+    const playBtn = document.querySelector(".play-icon");
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const currentVideoSlug = urlParams.get("v");
+    const currentVideo = recommendedVideos.find((v) => v.slug === currentVideoSlug);
+    const videoTitle = document.querySelector(".video-title");
+    
 
     const loadRecommendedVideos = () => {
         recommendedVideos.forEach(video => {
@@ -59,10 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
             </div>
             `
-            videoElement.addEventListener('click', () => {        
-                thumbnail.src = video.thumbnail;
-                thumbnail.style.display = "block";
-            
+            videoElement.addEventListener('click', () => {                    
                 loadVideo(video);
             })
 
@@ -71,19 +81,39 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const loadVideo = (video) => {
-        setTimeout(() => {
-            player.src = video.videoUrl; 
+        thumbnail.src = video.thumbnail;
+        thumbnail.style.display = "block";
+        videoTitle.textContent = video.title;
+
+        player.src = video.videoUrl;
+
+        player.addEventListener('loaded', () => {
+            thumbnail.style.display = "none";
+            playBtn.style.display = "none";
+
             player.load();
-        }, 3000)
+        })
+        
     }
 
     player.addEventListener('play', () => {
         thumbnail.style.display = "none";
+        playBtn.style.display = "none";
     })
 
-    thumbnail.classList.add("thumb-hidden");
+    player.addEventListener('pause', () => {
+        thumbnail.style.display = "block";
+        playBtn.style.display = "block";
+    })
+    
+    playBtn.addEventListener('click', () => {
+        thumbnail.style.display = "none";
+        playBtn.style.display = "none";
+        player.play();
+    })
 
     loadRecommendedVideos();
-    loadVideo(recommendedVideos[0]);
+    console.log(currentVideo);
+    loadVideo(currentVideo || recommendedVideos[0]);
 });
 
